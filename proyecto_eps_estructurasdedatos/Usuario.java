12b451fcab31e565e62c580949ae6164a25efa44
juguzmanp <juguzmanp@unal.edu.co>
2025-11-19 21:48:33 -0500

    Add files via upload

---
+++ proyecto_eps_estructurasdedatos/Usuario.java
@@ -0,0 +1,50 @@
+package com.mycompany.proyecto_eps_estructurasdedatos;
+
+public class Usuario {
+
+    private String tipoDocumento;
+    private String numeroDocumento;
+    private String nombres;
+    private String apellidos;
+    private int edad;
+    private String sexo;
+
+    public Usuario(String tipoDocumento, String numeroDocumento,
+                   String nombres, String apellidos, int edad, String sexo) {
+        this.tipoDocumento = tipoDocumento;
+        this.numeroDocumento = numeroDocumento;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.sexo = sexo;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    @Override
+    public String toString() {
+        return nombres + " " + apellidos + " (" + tipoDocumento + " " + numeroDocumento + ")";
+    }
+}
---
+++ proyecto_eps_estructurasdedatos/UsuarioUrgencias.java
@@ -0,0 +1,25 @@
+package com.mycompany.proyecto_eps_estructurasdedatos;
+
+public class UsuarioUrgencias extends Usuario{
+
+    private int nivelTriage;
+
+    public UsuarioUrgencias(Usuario base, int nivelTriage) {
+        super(base.getTipoDocumento(),
+                base.getNumeroDocumento(),
+                base.getNombres(),
+                base.getApellidos(),
+                base.getEdad(),
+                base.getSexo());
+        this.nivelTriage = nivelTriage;
+    }
+
+    public int getNivelTriage() {
+        return nivelTriage;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " - Nivel de triage " + nivelTriage;
+    }
+}
